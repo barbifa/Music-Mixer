5394ed9dfeac1abdc28d4ef3db260ab18dd1554d
barbifa <barbaraferrreiraaguiar@gmail.com>
2022-06-03 22:33:23 -0400

    drag working

--- script/main.js
+++ script/main.js
@@ -1,4 +1,17 @@
 (() => {
-	// set up 
+
 	
+	const icons = document.querySelectorAll('.relax-image');
+
+	icons.forEach(icon => icon.addEventListener('dragstart', dragStart));
+
+	console.log("js fired");
+
+
+
+	function dragStart(e) {
+		console.log('drag starts...');
+	 }
+
+
 })();
